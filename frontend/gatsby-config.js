dd756c042396e37a519d36d64e00279a01143b7d
Akira Takizawa <akx20000@protonmail.com>
2018-05-06 12:55:43 +0900

    Remove path prefix

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -2,7 +2,7 @@ const rupture = require("rupture");
 const nib = require("nib");
 
 module.exports = {
-  pathPrefix: '/project-name',
+  pathPrefix: '',
   siteMetadata: {
     title: 'Ethereum Social Network',
   },
